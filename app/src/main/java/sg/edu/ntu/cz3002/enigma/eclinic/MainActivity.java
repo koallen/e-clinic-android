97aa434c6eda1677b13055c3b53c3e2f02574f22
koallen <weifengzi2009@gmail.com>
2016-08-23 22:43:21 +0800

    bootstrapping

---
+++ app/src/main/java/sg/edu/ntu/cz3002/enigma/eclinic/MainActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.cz3002.enigma.eclinic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
