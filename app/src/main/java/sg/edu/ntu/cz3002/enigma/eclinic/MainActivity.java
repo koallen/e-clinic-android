783e8962db63c28ca1cfe49f7cfae4940b53e9fe
koallen <weifengzi2009@gmail.com>
2016-08-24 02:10:19 +0800

    add FCM

--- app/src/main/java/sg/edu/ntu/cz3002/enigma/eclinic/MainActivity.java
+++ app/src/main/java/sg/edu/ntu/cz3002/enigma/eclinic/MainActivity.java
@@ -2,6 +2,9 @@ package sg.edu.ntu.cz3002.enigma.eclinic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get device token for Firebase
+        String deviceToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("TOKEN", deviceToken);
     }
 }
