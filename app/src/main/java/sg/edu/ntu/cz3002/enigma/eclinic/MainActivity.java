f2d9ced8418a96c872145d3c02b61213b6570e2c
HuaBa <liuhualin333@gmail.com>
2016-08-30 22:08:13 +0800

    Preliminary login and sign up pages

--- app/src/main/java/sg/edu/ntu/cz3002/enigma/eclinic/MainActivity.java
+++ app/src/main/java/sg/edu/ntu/cz3002/enigma/eclinic/MainActivity.java
@@ -1,13 +1,16 @@
 package sg.edu.ntu.cz3002.enigma.eclinic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
         setContentView(R.layout.activity_main);
     }
 }
