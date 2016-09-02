bca6ca5a76d08f038c3001af1735ef8c5aa64ee0
ZWLori <lori961120@gmail.com>
2016-09-02 13:02:43 +0800

    bottom_bar implementation test

--- app/src/main/java/sg/edu/ntu/cz3002/enigma/eclinic/activity/MainActivity.java
+++ app/src/main/java/sg/edu/ntu/cz3002/enigma/eclinic/activity/MainActivity.java
@@ -1,18 +1,43 @@
 package sg.edu.ntu.cz3002.enigma.eclinic.activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
 
-import sg.edu.ntu.cz3002.enigma.eclinic.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private TextView messageView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_three_tabs);
+
+        messageView = (TextView) findViewById(R.id.messageView);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                messageView.setText(TabMessage.get(tabId, false));
+            }
+        });
+
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(@IdRes int tabId) {
+                Toast.makeText(getApplicationContext(), TabMessage.get(tabId, true), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
+
+
