5a44339048e604bd719313c599d61eb37c44e3e4
ZWLori <lori961120@gmail.com>
2016-09-13 23:47:17 +0800

    commit chatView

---
+++ app/src/main/java/sg/edu/ntu/cz3002/enigma/eclinic/view/ChatView.java
@@ -0,0 +1,11 @@
+package sg.edu.ntu.cz3002.enigma.eclinic.view;
+
+
+import com.hannesdorfmann.mosby.mvp.MvpView;
+
+/**
+ * Created by ZWL on 12/9/16.
+ */
+public interface ChatView extends MvpView {
+//    void sendMessage();
+}
