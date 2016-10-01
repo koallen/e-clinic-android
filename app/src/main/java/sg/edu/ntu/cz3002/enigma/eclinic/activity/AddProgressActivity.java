322776214e5a19505b1aeeb91e8409f1cb2d65b6
Allen-Shao <shaoyiyang312@hotmail.com>
2016-10-01 21:07:53 +0800

    new files

---
+++ app/src/main/java/sg/edu/ntu/cz3002/enigma/eclinic/activity/AddProgressActivity.java
@@ -0,0 +1,18 @@
+package sg.edu.ntu.cz3002.enigma.eclinic.activity;
+
+import android.widget.TextView;
+
+import butterknife.BindView;
+import sg.edu.ntu.cz3002.enigma.eclinic.R;
+
+/**
+ * Created by Allen on 2016/10/1.
+ */
+
+public class AddProgressActivity {
+    private static final String TAG = "AddReminderActivity";
+
+    @BindView(R.id.add_progress_content)
+    TextView _dateTextView;
+
+}
